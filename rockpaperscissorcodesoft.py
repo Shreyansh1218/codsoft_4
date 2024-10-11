627b6361da1d83a903136724c0634870734e90f2
Shreyansh1218 <149998934+Shreyansh1218@users.noreply.github.com>
2024-10-11 14:03:29 +0530

    Update rockpaperscissorcodesoft.py

--- rockpaperscissorcodesoft.py
+++ rockpaperscissorcodesoft.py
@@ -1,31 +1,68 @@
 
 import random
 
-options = ["Rock", "Paper", "Scissors"]
-user_choice = input("Select From The Following : Choose Rock, Paper ,Scissors: ")
-computer_choice = random.choice(options)
-print("Your Choice: ", user_choice)
-print("Computers Choice: ", computer_choice)
+while(True):
+    options = ["Rock", "Paper", "Scissors"]
+    user_choice = input("Select From The Following : Choose Rock, Paper ,Scissors: ")
+    computer_choice = random.choice(options)
+    print("Your Choice: ", user_choice)
+    print("Computers Choice: ", computer_choice)
 
  
-if user_choice == computer_choice:
+    if user_choice == computer_choice:
 
-    print("Same Choice Hence It's A Tie!")
+        print("Same Choice Hence It's A Tie!")
+        answer=input("Want To Play Again then type yes else type no:- ")
+        if answer=="no" :
+            break
+        else:
+            continue    
 
-elif user_choice == "Rock" and computer_choice == "Scissors":
+    elif user_choice == "Rock" and computer_choice == "Scissors":
 
-    print("You won!")
+        print("You won!")
+        answer=input("Want To Play Again :- ")
+        if answer=="no":
+            break
+        else:
+            continue
 
-elif user_choice == "Paper" and computer_choice == "Rock":
+    elif user_choice == "Paper" and computer_choice == "Rock":
 
-    print("You won!")
+        print("You won!")
+        answer=input("Want To Play Again :- ")
+        if answer=="no":
+            break
+        else:
+            continue
 
-elif user_choice == "Scissors" and computer_choice == "Paper":
+ 
+    elif user_choice == "Scissors" and computer_choice == "Paper":
 
-    print("You Won!")
+        print("You Won!")
+        answer=input("Want To Play Again :- ")
+        if answer=="no":
+            break
+        else:
+            continue
 
-else:
 
-    print("Computer Wins!")
+    else:
 
-print("If you Enjoyed then come back and challenge the computer again")   
+        print("Computer Wins!")
+        answer=input("Want To Play Again :- ")
+        if answer=="no":
+           break
+        else:
+           continue
+
+print("THANKYOU")   
+
+
+
+
+   
+
+
+
+
